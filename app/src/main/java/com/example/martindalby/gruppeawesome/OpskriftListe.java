4f43965273ef2570ce48bea8232392e101641c15
frederik <bgbg>
2016-11-07 17:09:12 +0100

    testdata

--- app/src/main/java/com/example/martindalby/gruppeawesome/OpskriftListe.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/OpskriftListe.java
@@ -1,13 +1,30 @@
 package com.example.martindalby.gruppeawesome;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class OpskriftListe extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        TestData[] data = new TestData[]{
+                new TestData("Pizza", "Denne ret smager godt"),
+                new TestData("Æggemad", "En lækker mad med æd... og brød"),
+                new TestData("Fisk", "Det er godt med fisk"),
+                new TestData("Æg", "Lidt af det gode"),
+                new TestData("Burger", "kød og kød og kød og kød..."),
+                new TestData("Salat", "Jk det er salat... med kød istedet for salat #Prot"),
+
+        };
+
+
+
+
         setContentView(R.layout.activity_opskrift_liste);
     }
 }
--- app/src/main/java/com/example/martindalby/gruppeawesome/TestData.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/TestData.java
@@ -1,6 +1,7 @@
 package com.example.martindalby.gruppeawesome;
 
 
+import android.media.Image;
 import android.widget.ImageView;
 
 public class TestData {
@@ -8,35 +9,12 @@ public class TestData {
     //her er data
     private String overskrift;
     private String beskrivelse;
-    private ImageView opskIcon;
+//    private Image opskIcon;
 
-    public TestData(String overskrift, ImageView opskIcon, String beskrivelse) {
+    public TestData(String overskrift, /*Image opskIcon,*/ String beskrivelse) {
         this.overskrift = overskrift;
-        this.opskIcon = opskIcon;
+ //       this.opskIcon = opskIcon;
         this.beskrivelse = beskrivelse;
     }
 
-    public String getOverskrift() {
-        return overskrift;
-    }
-
-    public void setOverskrift(String overskrift) {
-        this.overskrift = overskrift;
-    }
-
-    public String getBeskrivelse() {
-        return beskrivelse;
-    }
-
-    public void setBeskrivelse(String beskrivelse) {
-        this.beskrivelse = beskrivelse;
-    }
-
-    public ImageView getOpskIcon() {
-        return opskIcon;
-    }
-
-    public void setOpskIcon(ImageView opskIcon) {
-        this.opskIcon = opskIcon;
-    }
 }
