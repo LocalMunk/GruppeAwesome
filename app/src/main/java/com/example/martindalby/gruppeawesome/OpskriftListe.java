4ed1431662706899da475ac3ae82835fb0d50758
frederik <bgbg>
2016-11-07 16:34:02 +0100

    clean up rip ostefisk

--- app/src/main/java/com/example/martindalby/gruppeawesome/OpskriftListe.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/OpskriftListe.java
@@ -2,19 +2,12 @@ package com.example.martindalby.gruppeawesome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button knap;
+public class OpskriftListe extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        knap = (Button) findViewById(R.id.button2);
-
-
+        setContentView(R.layout.activity_opskrift_liste);
     }
 }
--- app/src/main/java/com/example/martindalby/gruppeawesome/TestData.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/TestData.java
@@ -1,6 +1,42 @@
 package com.example.martindalby.gruppeawesome;
 
 
+import android.widget.ImageView;
+
 public class TestData {
+
     //her er data
+    private String overskrift;
+    private String beskrivelse;
+    private ImageView opskIcon;
+
+    public TestData(String overskrift, ImageView opskIcon, String beskrivelse) {
+        this.overskrift = overskrift;
+        this.opskIcon = opskIcon;
+        this.beskrivelse = beskrivelse;
+    }
+
+    public String getOverskrift() {
+        return overskrift;
+    }
+
+    public void setOverskrift(String overskrift) {
+        this.overskrift = overskrift;
+    }
+
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
+
+    public void setBeskrivelse(String beskrivelse) {
+        this.beskrivelse = beskrivelse;
+    }
+
+    public ImageView getOpskIcon() {
+        return opskIcon;
+    }
+
+    public void setOpskIcon(ImageView opskIcon) {
+        this.opskIcon = opskIcon;
+    }
 }
