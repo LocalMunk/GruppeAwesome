27a91ca22e04f14a16a72575f9d2ad3781053d6c
Kristian Wiwe <wiwe123@hotmail.com>
2016-11-14 14:02:52 +0100

    Workout_frag - customadapter

--- app/src/main/java/com/example/martindalby/gruppeawesome/TestData.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/TestData.java
@@ -7,26 +7,34 @@ import android.widget.ImageView;
 public class TestData {
 
     //her er data
-    private String [] fisk = {"Kage","Snickers"};
-
+    private TestData [] data;
+    private  int imgRescourceId;
     private String text;
     private String overskrift;
     private String beskrivelse;
-//    private Image opskIcon;
+
+private int[] i ={R.drawable.pizzalistepic,R.drawable.grafbb};
 
     public  TestData(){
+        data = new TestData[]{
+                new TestData("Pizza", "Denne ret smager godt",R.drawable.pizzalistepic),
+                new TestData("Æggemad", "En lækker mad med æd... og brød",R.drawable.pizzalistepic),
+                new TestData("Fisk", "Det er godt med fisk",R.drawable.pizzalistepic),
+                new TestData("Æg", "Lidt af det gode",R.drawable.pizzalistepic),
+                new TestData("Burger", "kød og kød og kød og kød...",R.drawable.pizzalistepic),
+                new TestData("Salat", "Jk det er salat... med kød istedet for salat #Prot",R.drawable.pizzalistepic),
+        };
 
-        ;
     }
 
-    public TestData(String overskrift, /*Image opskIcon,*/ String beskrivelse) {
+    public TestData(String overskrift, String beskrivelse, int i) {
         this.overskrift = overskrift;
  //       this.opskIcon = opskIcon;
         this.beskrivelse = beskrivelse;
     }
 
-    public String[] getFisk() {
-        return fisk;
+    public TestData[] getdata() {
+        return data;
     }
 
     public String getOverskrift() {
