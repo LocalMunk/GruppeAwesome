e58b9eb79d79b3daf33f1199edb054ad6e845a72
Martin Dalby <martindalby@live.dk>
2016-11-17 08:18:26 +0100

    Startet arbejde på liste af øvelser

---
+++ app/src/main/java/com/example/martindalby/gruppeawesome/WorkoutList.java
@@ -0,0 +1,20 @@
+package com.example.martindalby.gruppeawesome;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Martin Dalby on 17-11-2016.
+ */
+
+public class WorkoutList extends AppCompatActivity implements View.OnClickListener {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    public void onClick(View v){
+
+    }
+}
