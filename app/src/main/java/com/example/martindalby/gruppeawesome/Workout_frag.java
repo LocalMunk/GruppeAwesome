1603f44f659f5b69a9061b5056b0cadc529f70d5
Alexander Winberg <Alexanderwinberg5611@gmail.com>
2016-11-21 09:41:15 +0100

    ændret grafen til søjle i workout_frag

--- app/src/main/java/com/example/martindalby/gruppeawesome/Workout_frag.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/Workout_frag.java
@@ -17,6 +17,7 @@ import android.widget.BaseAdapter;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
@@ -40,7 +41,7 @@ public class Workout_frag extends Fragment implements AdapterView.OnItemClickLis
         wdata = new TestDataWorkout();
 
         graph = (GraphView) rod.findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
@@ -48,21 +49,11 @@ public class Workout_frag extends Fragment implements AdapterView.OnItemClickLis
                 new DataPoint(4, 6)
         });
         graph.addSeries(series);
-
         // styling
-        //series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
-           // @Override
-            //public int get(DataPoint data) {
-              //  return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
-            //}
-        //});
-
-        //series.setSpacing(50);
-
-// draw values on top
-        //series.setDrawValuesOnTop(true);
-        //series.setValuesOnTopColor(Color.RED);
-//series.setValuesOnTopSize(50);
+        series.setSpacing(50);
+        // draw values on top
+        series.setDrawValuesOnTop(true);
+        series.setValuesOnTopColor(Color.RED);
 
 
         WorkoutAdapter adapter = new WorkoutAdapter(getActivity(), wdata.getWorkouts());
