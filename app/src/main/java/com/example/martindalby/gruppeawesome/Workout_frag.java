4b3d44324f0b261f03015f8de813beb04477d2f0
Alexander Winberg <Alexanderwinberg5611@gmail.com>
2016-11-21 08:58:31 +0100

    tilføjet graf til workout_frag

--- app/src/main/java/com/example/martindalby/gruppeawesome/Workout_frag.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/Workout_frag.java
@@ -51,6 +51,21 @@ public class Workout_frag extends Fragment implements AdapterView.OnItemClickLis
         });
         graph.addSeries(series);
 
+        // styling
+        //series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+           // @Override
+            //public int get(DataPoint data) {
+              //  return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
+            //}
+        //});
+
+        //series.setSpacing(50);
+
+// draw values on top
+        //series.setDrawValuesOnTop(true);
+        //series.setValuesOnTopColor(Color.RED);
+//series.setValuesOnTopSize(50);
+
 
         WorkoutAdapter adapter = new WorkoutAdapter(getActivity(), wdata.getWorkouts());
 
