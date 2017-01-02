a45585355ec12dd8c4a6fec69ff290da87d9fb32
Martin Dalby <martindalby@live.dk>
2017-01-02 12:10:22 +0100

    Tilføjet nye data klasser

--- app/src/main/java/com/example/martindalby/gruppeawesome/DataFiles/KostplanData.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/DataFiles/KostplanData.java
@@ -1,8 +1,16 @@
 package com.example.martindalby.gruppeawesome.DataFiles;
 
+import com.example.martindalby.gruppeawesome.Opskrift;
+
+import java.util.ArrayList;
+
 /**
  * Created by Martin Dalby on 02-01-2017.
  */
 
 public class KostplanData {
+    ArrayList<OpskriftData> morgen;
+    ArrayList<OpskriftData> frokost;
+    ArrayList<OpskriftData> aften;
+    ArrayList<OpskriftData> snacks;
 }
