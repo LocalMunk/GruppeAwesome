c1b8dfc51f900e7a6648555949fd3fe87b991c15
Martin Dalby <martindalby@live.dk>
2017-01-02 12:04:02 +0100

    Tilføjet nye data klasser

--- app/src/main/java/com/example/martindalby/gruppeawesome/DataFiles/TraeningsPlanData.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/DataFiles/TraeningsPlanData.java
@@ -1,8 +1,11 @@
 package com.example.martindalby.gruppeawesome.DataFiles;
 
+import java.util.ArrayList;
+
 /**
  * Created by Martin Dalby on 02-01-2017.
  */
 
 public class TraeningsPlanData {
+    ArrayList<WorkoutData> workouts;
 }
--- app/src/main/java/com/example/martindalby/gruppeawesome/DataFiles/WorkoutData.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/DataFiles/WorkoutData.java
@@ -1,8 +1,12 @@
 package com.example.martindalby.gruppeawesome.DataFiles;
 
+import java.util.ArrayList;
+
 /**
  * Created by Martin Dalby on 02-01-2017.
  */
 
 public class WorkoutData {
+    ArrayList<OvelseData> ovelser;
+
 }
