5c951951dada7e08aab620192fd033cdcf97987f
frederik <bgbg>
2017-01-02 12:17:14 +0100

    nu med meth (fejlrapportering)

--- app/src/main/java/com/example/martindalby/gruppeawesome/MainActivity.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/MainActivity.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
 
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity /*implements View.OnClickLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main2);
 
 
