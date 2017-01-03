33e5d8c82de37e6a87cd809e3798b6698f09854a
Kristian Wiwe <wiwe123@hotmail.com>
2017-01-03 13:46:53 +0100

    Added workout to the database Fixed the WorkoutData class

--- app/src/main/java/com/example/martindalby/gruppeawesome/DataFiles/WorkoutData.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/DataFiles/WorkoutData.java
@@ -6,19 +6,103 @@ import java.util.ArrayList;
  * Created by Martin Dalby on 02-01-2017.
  */
 
+
+
 public class WorkoutData {
-    ArrayList<OvelseData> ovelser;
+    private int ovelsesid,ovelsesid2,ovelsesid3,ovelsesid4,ovelsesid5,ovelsesid6,ovelsesid7,ovelsesid8;
+    private String workoutname;
+    private int workoutid;
 
-    public WorkoutData(ArrayList<OvelseData> ovelser){
-        this.ovelser = ovelser;
+
+    public WorkoutData(int workoutid, String workoutname,int ovelsesid, int ovelsesid2, int ovelsesid3, int ovelsesid4,int ovelsesid5, int ovelsesid6, int ovelsesid7, int ovelsesid8  ) {
+        this.ovelsesid2 = ovelsesid2;
+        this.ovelsesid3 = ovelsesid3;
+        this.ovelsesid4 = ovelsesid4;
+        this.ovelsesid5 = ovelsesid5;
+        this.ovelsesid6 = ovelsesid6;
+        this.ovelsesid7 = ovelsesid7;
+        this.ovelsesid8 = ovelsesid8;
+        this.ovelsesid = ovelsesid;
+        this.workoutid = workoutid;
+        this.workoutname = workoutname;
     }
 
-    public OvelseData getOvelse(int i){
-        return ovelser.get(i);
+    public int getOvelsesid2() {
+        return ovelsesid2;
     }
 
-    public void addOvelse(OvelseData od){
-        ovelser.add(od);
+    public void setOvelsesid2(int ovelsesid2) {
+        this.ovelsesid2 = ovelsesid2;
     }
 
+    public int getOvelsesid3() {
+        return ovelsesid3;
+    }
+
+    public void setOvelsesid3(int ovelsesid3) {
+        this.ovelsesid3 = ovelsesid3;
+    }
+
+    public int getOvelsesid4() {
+        return ovelsesid4;
+    }
+
+    public void setOvelsesid4(int ovelsesid4) {
+        this.ovelsesid4 = ovelsesid4;
+    }
+
+    public int getOvelsesid5() {
+        return ovelsesid5;
+    }
+
+    public void setOvelsesid5(int ovelsesid5) {
+        this.ovelsesid5 = ovelsesid5;
+    }
+    public int getOvelsesid6() {
+        return ovelsesid6;
+    }
+
+    public void setOvelsesid6(int ovelsesid6) {
+        this.ovelsesid6 = ovelsesid6;
+    }
+
+    public int getOvelsesid7() {
+        return ovelsesid7;
+    }
+
+    public void setOvelsesid7(int ovelsesid7) {
+        this.ovelsesid7 = ovelsesid7;
+    }
+
+    public int getOvelsesid8() {
+        return ovelsesid8;
+    }
+
+    public void setOvelsesid8(int ovelsesid8) {
+        this.ovelsesid8 = ovelsesid8;
+    }
+
+    public int getOvelsesid() {
+        return ovelsesid;
+    }
+
+    public void setOvelsesid(int ovelsesid) {
+        this.ovelsesid = ovelsesid;
+    }
+
+    public int getWorkoutid() {
+        return workoutid;
+    }
+
+    public void setWorkoutid(int workoutid) {
+        this.workoutid = workoutid;
+    }
+
+    public String getWorkoutname() {
+        return workoutname;
+    }
+
+    public void setWorkoutname(String workoutname) {
+        this.workoutname = workoutname;
+    }
 }
