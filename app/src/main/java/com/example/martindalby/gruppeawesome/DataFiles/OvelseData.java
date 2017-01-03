5bd06182367fd782d053d6a4752c0dc1e14e55f9
Martin Dalby <martindalby@live.dk>
2017-01-03 09:47:32 +0100

    tilføjet data til OvelseData

--- app/src/main/java/com/example/martindalby/gruppeawesome/DataFiles/OvelseData.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/DataFiles/OvelseData.java
@@ -6,11 +6,56 @@ package com.example.martindalby.gruppeawesome.DataFiles;
 
 public class OvelseData {
 
-    String navn;
-    boolean done;
-    Graf graf;
+    private String navn;
+    private boolean done;
+    private Graf graf;
+    private int sets, id;
 
-    OvelseData(){
+    public OvelseData(String navn, boolean done, Graf graf, int sets, int id) {
+        this.navn = navn;
+        this.done = done;
+        this.graf = graf;
+        this.sets = sets;
+        this.id = id;
+    }
 
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Graf getGraf() {
+        return graf;
+    }
+
+    public void setGraf(Graf graf) {
+        this.graf = graf;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
