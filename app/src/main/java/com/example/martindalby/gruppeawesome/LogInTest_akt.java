cb0744703667455df39b5c538e47ad6d440c7d3a
frederik <bgbg>
2017-01-06 12:25:41 +0100

    Magin Alrgin fix din login logik, yw btw

--- app/src/main/java/com/example/martindalby/gruppeawesome/LogInTest_akt.java
+++ app/src/main/java/com/example/martindalby/gruppeawesome/LogInTest_akt.java
@@ -1,10 +1,10 @@
 package com.example.martindalby.gruppeawesome;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,11 +15,7 @@ import com.example.martindalby.gruppeawesome.DataFiles.UserWorkoutData;
 
 import java.util.ArrayList;
 
-/**
- * Created by Martin Dalby on 06-01-2017.
- */
-
-public class LoginAktivitet  extends AppCompatActivity  implements View.OnClickListener{
+public class LogInTest_akt extends AppCompatActivity implements View.OnClickListener {
 
     Button sub, notsub;
     EditText bePeakedSubCode;
@@ -28,38 +24,38 @@ public class LoginAktivitet  extends AppCompatActivity  implements View.OnClickL
     String createdUserID;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_login);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_test);
         datafiles = MainController.getInstans();
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         sub = (Button) findViewById(R.id.buttoncontinuesub);
-        sub.setOnClickListener(this);
         sub.setText("Fortsæt");
 
         notsub = (Button) findViewById(R.id.buttonnotsubbed);
-        notsub.setOnClickListener(this);
         notsub.setText("Fortsæt som gratis bruger");
 
         bePeakedSubCode = (EditText) findViewById(R.id.bepeakedsubcode);
         bePeakedSubCode.setHint("Indtast din BePeaked kode");
+
+        sub.setOnClickListener(this);
+        notsub.setOnClickListener(this);
     }
+
 
     @Override
     public void onClick(View v) {
-        if(v == sub && bePeakedSubCode.getText().toString().equals("") == false) {
+        if (v == sub && bePeakedSubCode.getText().toString().equals("") == false) {
             sharedPreferences.edit().putString("UserID", bePeakedSubCode.getText().toString()).commit();
-
             finish();
-        }
-        else if(v == notsub){
+
+        } else if (v == notsub) {
             sharedPreferences.edit().putString("UserID", datafiles.generateUserKey()).commit();
             sharedPreferences.getString("userID", createdUserID);
-            datafiles.pushUser(new Bruger(createdUserID, new ArrayList< UserWorkoutData>(), new ArrayList<String>()));
+            datafiles.pushUser(new Bruger(createdUserID, new ArrayList<UserWorkoutData>(), new ArrayList<String>()));
             finish();
 
         }
     }
-
 }
